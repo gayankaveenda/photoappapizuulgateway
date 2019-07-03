591c4d5509069d1dabcfef0ff4f4602fa29ed2e3
Gayan Kaveenda <gayankaveenda@gmail.com>
2019-07-03 23:24:53 +1000

    added zuul load balancer

---
+++ src/test/java/com/developer/photoapp/zuul/photoappapizuulgateway/PhotoappapizuulgatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.developer.photoapp.zuul.photoappapizuulgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoappapizuulgatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
